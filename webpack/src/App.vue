4da8f10a193bd5b04e4ac17141d3caebfa12f19e
David <Arrisdw@163.com>
2023-09-17 00:18:03 +0800

    webpack
    webpack demo

---
+++ webpack/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="name-class">{{ name }}</div>
+</template>
+
+<script setup lang="ts">
+import {ref,reactive} from 'vue'
+const name  = ref<string>('webpack success!')
+</script>
+
+<style scoped lang="less">
+.name-class{
+    background-color: #239861;
+    color: #fff;
+}
+</style>
